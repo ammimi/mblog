d63753c84dc60e6b48a2b006726cfb2d25a5a2c6
“mummywho” <wuguifeng0401@163.com>
2015-03-11 11:15:49 +0800

    add static

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mblog.settings")
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))#3
+import sys # 4
+sys.path.insert(0,PROJECT_DIR) # 5
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "mblog.settings"
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mblog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
